24cf680ec59053548af8ae2d48307a0f22506e6a
Alexei Huryanchyk <alexeihuryanchyk@cloudbusinesshq.com>
2021-02-12 22:56:56 +0300

    Dollar side effects

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,19 @@
+package guru.springframework;
+
+/**
+ * @author Aliaksei Huryanchyk
+ */
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+
+}
